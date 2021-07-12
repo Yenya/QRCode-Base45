4b69ec665e12efe0829ce3d03cfa52a67352ef3f
Jan "Yenya" Kasprzak <kas@fi.muni.cz>
2021-07-12 13:35:16 +0200

    Initial boilerplate generated by module-starter
    $ module-starter --module=QRCode::Base45 \
    	--author='Jan "Yenya" Kasprzak' \
    	--email=kas@fi.muni.cz --minperl=5.10.0 \
    	--license=perl --genlicense

---
+++ lib/QRCode/Base45.pm
@@ -0,0 +1,104 @@
+package QRCode::Base45;
+
+use 5.10.0;
+use strict;
+use warnings;
+
+=head1 NAME
+
+QRCode::Base45 - The great new QRCode::Base45!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use QRCode::Base45;
+
+    my $foo = QRCode::Base45->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Jan "Yenya" Kasprzak, C<< <kas at fi.muni.cz> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-qrcode-base45 at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=QRCode-Base45>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc QRCode::Base45
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=QRCode-Base45>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/QRCode-Base45>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/QRCode-Base45>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is copyright (c) 2021 by Jan "Yenya" Kasprzak.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
+=cut
+
+1; # End of QRCode::Base45
