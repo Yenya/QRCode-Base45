4daaec30c385d28dc7bb6a8117340ba8b4ae25ff
Jan "Yenya" Kasprzak <kas@fi.muni.cz>
2021-07-13 10:06:11 +0200

    Documentation fixes

--- README.md
+++ README.md
@@ -10,46 +10,61 @@ Version 0.01
 
     use QRCode::Base45;
 
-    my $text_for_qrcode = encode_base45($binary_data);
+    my $text_for_qrcode = encode_base45($binary_or_utf8_data);
     my $binary_data = decode_base45($text_from_qrcode);
 
 # DESCRIPTION
 
-This module handles encoding/decoding Base45 data,
+This module handles encoding/decoding of Base45 data,
 as described in
 [draft-faltstrom-base45-06](http://www.watersprings.org/pub/id/draft-faltstrom-base45-06.html).
-Base45 is used especially in QR codes.
+Base45 is used especially in QR codes, for example in European vaccination
+certificates.
 
 ## encode\_base45
 
-Takes arbitrary string as argument, and returs the Base45 representation
+Takes an arbitrary string as argument, and returns the Base45 representation
 of it. Character strings (as opposed to byte strings) are encoded to bytes
-as UTF-8 first.
+as UTF-8.
 
-For zero-length strings (undef or '') the empty string is returned.
+For zero-length input strings (undef or '') an empty string ('') is returned.
 
 ## decode\_base45
 
-The function takes a textual base45 representation of data,
-and tries to decode it. Returned value is a byte string (as this function
-cannot know whether the contents should be interpreted as bytes or UTF-8
-data (or whatever else). The caller has to decode the characters afterwards,
+Takes a textual Base45 representation of data, and tries to decode it.
+Returned value is a byte string (as this function cannot possibly know
+whether the content should be interpreted as bytes or UTF-8).
+The caller has to decode the returned byte string to characters afterwards,
 if needed.
 
-For zero-length input, the empty string is returned.
+For zero-length input strings (undef or '') an empty string ('') is returned.
 
 For invalid inputs, such as strings of length 3n+1 or characters
 outside of the Base45 alphabet, this function croak()s.
 
 # AUTHOR
 
-Jan "Yenya" Kasprzak, `<kas at fi.muni.cz>`
+Jan "Yenya" Kasprzak, `<kas at yenya.net>`
 
 # BUGS
 
 Please report any bugs or feature requests to `bug-qrcode-base45 at rt.cpan.org`, or through
 the web interface at [https://rt.cpan.org/NoAuth/ReportBug.html?Queue=QRCode-Base45](https://rt.cpan.org/NoAuth/ReportBug.html?Queue=QRCode-Base45).  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
+
+The Base45 encoding is relatively new. After it is standardized
+and maybe used elsewhere apart from QR codes,
+this module should probably be moved to some other namespace,
+such as IETF:: or RFCxyzq::.
+
+# INSTALLATION
+
+To install this module, run the following commands:
+
+        perl Makefile.PL
+        make
+        make test
+        make install
 
 # SUPPORT
 
@@ -58,6 +73,10 @@ You can find documentation for this module with the perldoc command.
     perldoc QRCode::Base45
 
 You can also look for information at:
+
+- Github repository
+
+    [https://github.com/Yenya/QRCode-Base45](https://github.com/Yenya/QRCode-Base45)
 
 - RT: CPAN's request tracker (report bugs here)
 
--- lib/QRCode/Base45.pm
+++ lib/QRCode/Base45.pm
@@ -29,23 +29,24 @@ our $ALPHABET = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:';
 
     use QRCode::Base45;
 
-    my $text_for_qrcode = encode_base45($binary_data);
+    my $text_for_qrcode = encode_base45($binary_or_utf8_data);
     my $binary_data = decode_base45($text_from_qrcode);
 
 =head1 DESCRIPTION
 
-This module handles encoding/decoding Base45 data,
+This module handles encoding/decoding of Base45 data,
 as described in
 L<draft-faltstrom-base45-06|http://www.watersprings.org/pub/id/draft-faltstrom-base45-06.html>.
-Base45 is used especially in QR codes.
+Base45 is used especially in QR codes, for example in European vaccination
+certificates.
 
 =head2 encode_base45
 
-Takes arbitrary string as argument, and returs the Base45 representation
+Takes an arbitrary string as argument, and returns the Base45 representation
 of it. Character strings (as opposed to byte strings) are encoded to bytes
-as UTF-8 first.
+as UTF-8.
 
-For zero-length strings (undef or '') the empty string is returned.
+For zero-length input strings (undef or '') an empty string ('') is returned.
 
 =cut
 
@@ -76,13 +77,13 @@ sub encode_base45 {
 
 =head2 decode_base45
 
-The function takes a textual base45 representation of data,
-and tries to decode it. Returned value is a byte string (as this function
-cannot know whether the contents should be interpreted as bytes or UTF-8
-data (or whatever else). The caller has to decode the characters afterwards,
+Takes a textual Base45 representation of data, and tries to decode it.
+Returned value is a byte string (as this function cannot possibly know
+whether the content should be interpreted as bytes or UTF-8).
+The caller has to decode the returned byte string to characters afterwards,
 if needed.
 
-For zero-length input, the empty string is returned.
+For zero-length input strings (undef or '') an empty string ('') is returned.
 
 For invalid inputs, such as strings of length 3n+1 or characters
 outside of the Base45 alphabet, this function croak()s.
@@ -123,7 +124,7 @@ sub decode_base45 {
 
 =head1 AUTHOR
 
-Jan "Yenya" Kasprzak, C<< <kas at fi.muni.cz> >>
+Jan "Yenya" Kasprzak, C<< <kas at yenya.net> >>
 
 =head1 BUGS
 
@@ -131,6 +132,19 @@ Please report any bugs or feature requests to C<bug-qrcode-base45 at rt.cpan.org
 the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=QRCode-Base45>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
 
+The Base45 encoding is relatively new. After it is standardized
+and maybe used elsewhere apart from QR codes,
+this module should probably be moved to some other namespace,
+such as IETF:: or RFCxyzq::.
+
+=head1 INSTALLATION
+
+To install this module, run the following commands:
+
+	perl Makefile.PL
+	make
+	make test
+	make install
 
 =head1 SUPPORT
 
@@ -142,6 +156,10 @@ You can find documentation for this module with the perldoc command.
 You can also look for information at:
 
 =over 4
+
+=item * Github repository
+
+L<https://github.com/Yenya/QRCode-Base45>
 
 =item * RT: CPAN's request tracker (report bugs here)
 
